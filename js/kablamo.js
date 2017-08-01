e87ae734d42d21d181853d8255c6272205701209
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-07-31 23:44:59 -0700

    Create basic level and add mouse constraint

--- js/kablamo.js
+++ js/kablamo.js
@@ -1,8 +1,74 @@
-document.addEventListener("DOMContentLoaded", function(){
-  const canvasEl = document.getElementById('canvas');
-  var stage = new createjs.Stage(canvasEl);
-  var shape = new createjs.Shape();
-  shape.graphics.beginFill('red').drawRect(0, 0, 120, 120);
-  stage.addChild(shape);
-  stage.update();
+document.addEventListener("DOMContentLoaded", function() {
+  var Engine = Matter.Engine,
+    Events = Matter.Events,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Body = Matter.Body,
+    Common = Matter.Common,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    Constraint = Matter.Constraint,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Vector = Matter.Vector;
+
+  // create an engine
+  var engine = Engine.create(),
+    world = engine.world;
+
+  // create two boxes and a ground
+  var body = Bodies.circle(400, 200, 40);
+  var ground = Bodies.rectangle(600, 610, 1000, 20, { isStatic: true });
+  var leftWall = Bodies.rectangle(120, 350, 20, 500, { isStatic: true });
+  var rightWall = Bodies.rectangle(1080, 350, 20, 500, { isStatic: true });
+  var roof = Bodies.rectangle(600, 100, 1000, 20, { isStatic: true });
+
+  // add all of the bodies to the world
+  World.add(world, [body, ground, leftWall, rightWall, roof]);
+
+  // run the engine
+  Engine.run(engine);
+
+  var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      wireframes: false,
+      width: 1200,
+      height: 700,
+      showShadows: true,
+      showCollisions: true,
+      pixelRatio: "auto",
+      showAngleIndicator: true
+    }
+  });
+
+  Render.run(render);
+
+  // create runner
+  var runner = Runner.create();
+  Runner.run(runner, engine);
+
+  // add mouse control and make the mouse revolute
+  var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.6,
+        length: 0,
+        angularStiffness: 0,
+        render: {
+          visible: true
+        }
+      }
+    });
+
+  World.add(world, mouseConstraint);
+
+  // keep the mouse in sync with rendering
+  render.mouse = mouse;
+
+  mouseConstraint.body = body;
 });
